5bba26c7d67eb4a17ea8ac1584340bdc5fe875c7
Kenya Womack <ladykamillion27@gmail.com>
2023-05-24 16:47:38 -0500

    update file structure and server codes

--- index.js
+++ index.js
@@ -1,9 +1,7 @@
 'use strict';
-
 require('dotenv').config();
-const app = require('./src/server.js');
-const { db } = require('./src/auth/models');
-
+const app = require('./src/server');
+const { db } = require('./src/models/index');
 db.sync().then(() => {
   app.start(process.env.PORT || 3001);
 });
--- src/server.js
+++ src/server.js
@@ -4,9 +4,10 @@
 const express = require('express');
 const cors = require('cors');
 
-
 // Esoteric Resources
+const notFoundHandler = require('./error-handlers/404.js');
 const errorHandler = require('./error-handlers/500.js');
+const logger = require('./middleware/logger.js');
 const notFound = require('./error-handlers/404.js');
 const authRoutes = require('./auth/routes.js');
 
@@ -16,15 +17,33 @@ const app = express();
 // App Level MW
 app.use(cors());
 
+const v1Routes = require('./routes/v1.js');
+
+
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
 // Routes
 app.use(authRoutes);
 
+app.use(logger);
+
+app.use('/api/v1', v1Routes);
+
+app.use('*', notFoundHandler);
+app.use(errorHandler);
+
 // Catchalls
 app.use(notFound);
 app.use(errorHandler);
+
+module.exports = {
+  server: app,
+  start: port => {
+    if (!port) { throw new Error('Missing Port'); }
+    app.listen(port, () => console.log(`Listening on ${port}`));
+  },
+};
 
 module.exports = {
   server: app,
